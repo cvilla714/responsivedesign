6973c7436b32be21a229357e446a7695230fe8b4
Cosmel <62273200+cvilla714@users.noreply.github.com>
2020-09-09 22:28:43 -0600

    finished implementing jquery, created the functions to add the padding on the left and insert the logo,also  created the first media query for the grid and the sticky nav bar

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("hello my friend");
+
+$(window).scroll(function () {
+  let scroll = $(window).scrollTop();
+  //console.log(scroll);
+  if (scroll >= 5) {
+    //console.log('a');
+    $(".right_sticky5 a").addClass("push_left");
+  } else {
+    //console.log('a');
+    $(".right_sticky5 a").removeClass("push_left");
+  }
+});
+
+$(window).scroll(function () {
+  let scrolling = $(window).scrollTop();
+  //console.log(scroll);
+  if (scrolling >= 5) {
+    //console.log('a');
+    $(".main").addClass("push_left");
+  } else {
+    //console.log('a');
+    $(".main").removeClass("push_left");
+  }
+});
+
+$(window).scroll(function () {
+  let scrollimg = $(window).scrollTop();
+  // console.log(scroll);
+  if (scrollimg >= 5) {
+    $(".logo_img").addClass("logo_display");
+  } else {
+    // console.log("a");
+    $(".logo_img").removeClass("logo_display");
+  }
+});
+
+$(function () {
+  $(".news").menu();
+});
